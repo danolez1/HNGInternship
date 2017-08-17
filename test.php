f44ba3e1483b130f4a019101cb92520e4babc2e6
Tunmise Fatunmbi <danolez1fatuns@gmail.com>
2017-08-17 18:17:30 -0400

    Create test.php

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+define("HOST_NAME","localhost");
+define("USERNAME","root");
+define("PASSWORD","password");
+define("DB_NAME","HNGIntership");
+
+$conn = new mysqli(HOST_NAME, USERNAME, PASSWORD,DB_NAME);
+
