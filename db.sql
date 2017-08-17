a3603a1325c8a9b2a938a77d358e96c68c851a56
Tunmise Fatunmbi <danolez1fatuns@gmail.com>
2017-08-17 18:34:38 -0400

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE HNGIntership;
+CREATE TABLE `HNGIntership`.`test` ( `id` INT NOT NULL AUTO_INCREMENT ,  `username` VARCHAR(20) NOT NULL ,  `password` VARCHAR(20) NOT NULL ,  `date` INT(20) NOT NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;
+INSERT INTO `test` (`id`, `username`, `password`, `date`) VALUES (NULL, 'danolez1fatuns', 'password', '1503008780');
