87774105e23ca3f020c05d06f7e50fd85a19f8cb
Tunmise Fatunmbi <danolez1fatuns@gmail.com>
2017-08-17 18:32:18 -0400

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,18 @@ define("HOST_NAME","localhost");
 define("USERNAME","root");
 define("PASSWORD","password");
 define("DB_NAME","HNGIntership");
+define("TEST_TABLE","test");
 
 $conn = new mysqli(HOST_NAME, USERNAME, PASSWORD,DB_NAME);
+
+$query = "SELECT * FROM ".TEST_TABLE;
+$stmt = $conn->query($query);
+$result = array();
+if ($stmt){
+	while($row = $stmt->fetch_assoc()){
+	array_push($result, $row);
+	}
+	echo json_encode(array("response"=>$result));
+
+}
 
